fa96a7ab3b5087c9efedc601f07b053f829a6fcd
isnanms <morningsick2@gmail.com>
2025-04-05 18:52:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,10 @@ import gspread
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 import json
-import pandas as pd
 
-# Set up Google Sheets connection using Streamlit secrets
 def authenticate_google_sheets():
-    # Ambil JSON kredensial dari Secrets Streamlit
-    creds_json = st.secrets["google_service_account"]  # Sesuaikan dengan nama secret kamu
-
-    # Pastikan creds_json adalah string, jika bukan, ubah terlebih dahulu
-    if isinstance(creds_json, dict):
-        creds_json = json.dumps(creds_json)  # Jika berupa dictionary, ubah menjadi string JSON
-
-    # Jika kredensial sudah berupa string JSON, ubah menjadi dictionary Python
-    creds_dict = json.loads(creds_json)
+    # Ambil kredensial dari Streamlit Secrets
+    creds_dict = st.secrets["google_service_account"]
 
     # Gunakan kredensial langsung dari dictionary
     creds = Credentials.from_service_account_info(
@@ -25,15 +16,13 @@ def authenticate_google_sheets():
 
     return gspread.authorize(creds)
 
-# Get data from Google Sheets
 def get_family_data():
     client = authenticate_google_sheets()
     sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1__VDkWvS-FdSHpOhNnLvqej4ggFKU1xqJnobDlTeppc/edit?gid=0#gid=0")
-    worksheet = sheet.get_worksheet(0)  # Access the first sheet
+    worksheet = sheet.get_worksheet(0)  # Akses sheet pertama
     data = pd.DataFrame(worksheet.get_all_records())
     return data
 
-# Main function to run the app
 def main():
     st.title("Pohon Keluarga")
     st.write("Aplikasi ini menampilkan pohon keluarga berdasarkan data di Google Sheets.")
